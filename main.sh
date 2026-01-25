460cb3ec450cbd0f3fdb5e1beddef4de9f158abe
UsefShaheen <usef.m.shaheen@gmail.com>
2026-01-26 00:52:53 +0200

    feat: add create database functionality and basic menu

---
+++ main.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DB_DIR="./databases"
+
+mkdir -p "$DB_DIR"
+
+
+function main_menu(){
+	echo "====== Bash DBMS ======"
+	echo "1) Create Database"
+	echo "2) List Database"
+	echo "3) Connect To Database"
+	echo "4) Drop Database"
+	echo "5) Exit"
+	read -p "Choose an option: "choice
+
+	case $choice in 
+		1) create_database ;;
+		2) list_databases ;;
+		3) connect_database ;;
+		4) drop_database ;;
+		5) exit 0 ;;
+		*) echo "Invalid option"; main_menu ;;
+	esac	
+}
+
+
+function create_database(){
+	read -p "Enter database name: " db_name
+	if [[ -d "$DB_DIR/$db_name" ]]; then
+		echo "Database already exists!"
+	else
+		mkdir "$DB_DIR/$db_name"
+		echo "Database '$db_name' created successfully."
+	fi
+	main_menu
+}
+
+function list_database(){
+	echo "Databases:"
+	ls "$DB_DIR"
+	main_menu
+}
+
+
+
+function connect_database() {
+    echo "Not implemented yet."
+    main_menu
+}
+
+function drop_database() {
+    echo "Not implemented yet."
+    main_menu
+}
+
+# Start the app
+main_menu
